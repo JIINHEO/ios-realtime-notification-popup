6674343f2d6d2ee42fad012ceb433c83ad36839b
JIINHEO <gjwldls123@gmail.com>
2021-11-22 23:24:45 +0900

    [#3] Feat: A_B Test로 팝업 제어

--- ios-realtime-notification-popup/ViewController.swift
+++ ios-realtime-notification-popup/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseRemoteConfig
+import FirebaseAnalytics
 
 class ViewController: UIViewController {
     
@@ -55,8 +56,8 @@ extension ViewController {
                 
                 noticeVC.noticeContents = (title: title, detail: detail, date: date)
                 self.present(noticeVC, animated: true, completion: nil)
-        
-                
+            } else {
+                self.showEventAlert()
             }
         }
     }
@@ -65,6 +66,31 @@ extension ViewController {
         return remoteConfig["isHidden"].boolValue
     }
     
-    
+}
+//A/B Testing
+extension ViewController {
+    func showEventAlert() {
+        guard let remoteConfig = remoteConfig else {return}
+        remoteConfig.fetch {[weak self] status, _ in
+            if status == .success {
+                remoteConfig.activate(completion: nil)
+            } else {
+                print("Config not fetched")
+            }
+            let message = remoteConfig["message"].stringValue ?? ""
+
+            let confirmAction = UIAlertAction(title: "확인하기", style: .default) { sdf in
+                Analytics.logEvent("promotion_alert", parameters: nil)
+            }
+
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            let alertController = UIAlertController(title: "깜짝이벤트", message: message, preferredStyle: .alert)
+            
+            alertController.addAction(confirmAction)
+            alertController.addAction(cancelAction)
+            
+            self?.present(alertController, animated: true, completion: nil)
+        }
+    }
 }
 
